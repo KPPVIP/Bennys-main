ec5041f1204fddc68c3be4e4a77072e5d094829b
Guudz <Kilian1702@outlook.com>
2023-02-02 04:04:39 +0100

    Initial commit

---
+++ bennylift/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+
+-- data_file('DLC_ITYP_REQUEST')('stream/shield.ytyp')
+
+ui_page 'html/index.html'
+
+
+client_script {
+  'client.lua',
+}
+
+server_script {
+  'server.lua',
+}
